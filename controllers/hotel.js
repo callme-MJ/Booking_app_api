4ae47b1fd40be3edc5dd5bd05a926bd9edd31729
Muhammed Jabir <jabirjbr07@gmail.com>
2023-04-08 15:32:20 +0530

    error handled

---
+++ controllers/hotel.js
@@ -0,0 +1,1 @@
+export const createHotel = async 
---
+++ index.js
@@ -0,0 +1,49 @@
+import  express  from "express";
+const app = express();
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js"
+import hotelsRoute from "./routes/hotels.js"
+import roomsRoute from "./routes/rooms.js"
+dotenv.config();
+
+const connect = async ()=>{
+
+    try{
+        await mongoose.connect(process.env.MONGO)
+        console.log("MongoDB connected")
+    }catch (err){
+        console.log(err)
+    }
+}
+
+mongoose.connection.on("disconnected", () => {
+    console.log("MongoDB disconnected");
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.use(express.json())
+app.use("/api/auth", authRoute)
+app.use("/api/users", usersRoute)
+app.use("/api/hotels", hotelsRoute)
+app.use("/api/roooms", roomsRoute)
+
+app.use((err,req,res,next)=>{
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || "Something went wrong!"
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    });
+})
+
+app.listen(3001, () => {
+    connect();
+  console.log("Server is running on port 3000");
+});
