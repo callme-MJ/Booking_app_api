0e4f0ea68f018c8151f982a6004cae747c873380
Muhammed Jabir <jabirjbr07@gmail.com>
2023-04-09 14:27:50 +0530

    api cruds done

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,6 +1,19 @@
 import express from "express";
 const router = express.Router();
+import { createRoom, deleteRoom, getRoom, getRooms, updateeRoom } from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
+
+
+router.post("/:hotelid",verifyAdmin, createRoom);
+
+router.put("/:id",verifyAdmin, updateeRoom)
+
+router.delete("/:id/:hotelid",verifyAdmin, deleteRoom)
+
+router.get("/:id",getRoom)
+
+router.get("/", getRooms)
 
 
 export default router;
